3a285cc810a5d4fe467856b04f9a9a35ca8cccf9
Max Horowitz <maxhorowitz@gmail.com>
2023-09-04 01:09:41 -0400

    connected server and client

---
+++ makefile
@@ -0,0 +1,7 @@
+latest := 1
+
+update:
+	npm update
+
+run:
+	node server_v${latest}.js
---
+++ server_v1.js
@@ -0,0 +1,32 @@
+const EventEmitter = require('events');
+
+var messages = require('grpc-go-node-example-api/pb/proto/service_pb');
+var services = require('grpc-go-node-example-api/pb/proto/service_grpc_pb');
+var grpc = require('@grpc/grpc-js');
+
+/**
+ * @param {firstName} first Request object for the handler to process
+ * @return {lastName}
+ */
+function checkLast(first) {
+    console.log(first.getName());
+    last = new messages.LastName();
+    last.setName("horowitz")
+    return last;
+}
+
+/**
+ * @param {EventEmitter} call Call object for the handler to process
+ * @param {function(Error, lastName)} callback Response callback
+ */
+function implementationGetLast(call, callback){
+    callback(null, checkLast(call.request));
+}
+
+var server = new grpc.Server();
+server.addService(services.RegistryService, {
+    getLast: implementationGetLast,
+});
+server.bindAsync('localhost:8080', grpc.ServerCredentials.createInsecure(), () => {
+    server.start();
+});
